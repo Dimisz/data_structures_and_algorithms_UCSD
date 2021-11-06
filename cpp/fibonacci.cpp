8a8e28a86dd19e5b349e97272148e0d35c518b32
Vladimir Solovyov <leilyngale@icloud.com>
2021-11-06 20:14:58 +0800

    fibonacci sequence

---
+++ cpp/fibonacci.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+unsigned long long fibonacci(int n){
+  vector<unsigned long long> fib_nums{0, 1};
+  
+  for (int i = 1; i < n; i++){
+    int last_index, before_last_index;
+    
+    last_index = fib_nums.size() - 1;
+    before_last_index = fib_nums.size() - 2;
+    
+    fib_nums.push_back((fib_nums[before_last_index] + fib_nums[last_index]));
+    
+    //cout << fib_nums[last_index] << endl;
+ }
+  
+return fib_nums[n-1];
+}
+
+int main(){
+  cout << "Enter the number in Fibonacci sequence you would like to see: " << endl;
+  int n;
+  cin >> n;
+  //fibonacci(n);
+  unsigned long long answer = fibonacci(n);
+  cout << "Number " << n << " in Fibonacci sequence is " << answer << endl;
+  
+  return 0;
+}
